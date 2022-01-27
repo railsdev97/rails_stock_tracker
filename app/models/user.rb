c20d94d70faecc60f6f998e84b457a04d0852a19
railsdev97 <railsdev97@gmail.com>
2022-01-27 11:02:52 +0700

    [FriendShip] Add friendship model and implement search users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -19,6 +21,21 @@ class User < ApplicationRecord
 
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = matches('email', param)
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
   end
   
 end
