d3e5e4db6c2a5b6e074b29c865ad392cebbe4d2b
railsdev97 <railsdev97@gmail.com>
2022-01-27 15:11:15 +0700

    [FriendShip] Implement follow friend functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,14 @@
 class FriendshipsController < ApplicationController
 
   def create
+    if params[:friend]
+      friend = User.find(params[:friend])
+      current_user.friends << friend
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was something wrong"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
